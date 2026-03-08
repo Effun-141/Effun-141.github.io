88915411999f1bd48d124117bff10c2c1e0c5df9
Effun-141 <48988961+Effun-141@users.noreply.github.com>
2026-03-08 09:33:04 +0000

    Deploying to gh-pages from @ Effun-141/Effun-141.github.io@7b9cac8c40a67fe43acb7ea278c354b94740a28f 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://alshedivat.github.io/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://effun.xyz/#contact">contact us</a>.'
               }
             ]
           }
