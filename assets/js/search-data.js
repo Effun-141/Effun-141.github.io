2a193eafe16b5c74a4ca3a9aa44582bb5d7bb46a
Effun-141 <48988961+Effun-141@users.noreply.github.com>
2026-03-08 16:34:20 +0000

    Deploying to gh-pages from @ Effun-141/Effun-141.github.io@fc0c8aaaef3caedd93fc6ffd95a896d4da846302 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -441,7 +441,7 @@ ninja.data = [{
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
             },},{id: "news-a-paper-was-accepted-by-2026-ieee-international-conference-on-robotics-and-automation-icra",
-          title: 'A paper was accepted by 2026 IEEE International Conference on Robotics and Automation...',
+          title: '🎉 A paper was accepted by 2026 IEEE International Conference on Robotics and...',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
