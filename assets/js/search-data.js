05e8080791068dbd7deb80cd157788bc58bb4413
Effun-141 <48988961+Effun-141@users.noreply.github.com>
2026-03-09 03:15:50 +0000

    Deploying to gh-pages from @ Effun-141/Effun-141.github.io@00b7b828b994363c5aa0b38c37c7d4a3d07ba7c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
