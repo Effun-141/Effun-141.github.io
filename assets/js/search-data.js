7d918e5df0a43f30e017705e25392ed70ae18383
Effun-141 <48988961+Effun-141@users.noreply.github.com>
2026-03-17 07:34:48 +0000

    Deploying to gh-pages from @ Effun-141/Effun-141.github.io@c6525a4cd8ae2f1bdcd7fd81adb7fb685ec34a6f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
