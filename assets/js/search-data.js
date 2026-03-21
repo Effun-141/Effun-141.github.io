d8328d8b9e5f464c151cfd233b9076a35ad82cbe
Effun-141 <48988961+Effun-141@users.noreply.github.com>
2026-03-21 16:33:59 +0000

    Deploying to gh-pages from @ Effun-141/Effun-141.github.io@90136592eea28e76ac9f9caaff5fd7de9e08b2d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -528,9 +528,9 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-dynamic-visual-slam",
+          title: 'Dynamic Visual SLAM',
+          description: "Work during Master stage",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
