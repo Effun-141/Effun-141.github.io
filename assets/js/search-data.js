3484dcc554eb8b7de28408cfe270e7bf8e110635
Effun-141 <48988961+Effun-141@users.noreply.github.com>
2026-03-21 16:11:07 +0000

    Deploying to gh-pages from @ Effun-141/Effun-141.github.io@986f515795903e1738764fc49181a81456a3115d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "整理中，coming soon",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
