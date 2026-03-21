8bbb52c35e6ed4576d98b045878cce382a40d7ca
Effun-141 <48988961+Effun-141@users.noreply.github.com>
2026-03-21 16:11:59 +0000

    Deploying to gh-pages from @ Effun-141/Effun-141.github.io@2d5448fdce40f6c6bcf6903d4b94e2c42c4cd897 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,7 +38,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "Course materials, schedules, and resources for classes taught.",
           section: "Navigation",
           handler: () => {
